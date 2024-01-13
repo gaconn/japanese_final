f23fd4d1249de70b9a8e188cf09191d67a93397c
Đồng Quân <quan12xz@gmail.com>
2024-01-13 22:26:23 +0700

    fix scroll

--- frontend/app/src/view/page/Hiragana/HiraganaTheory.js
+++ frontend/app/src/view/page/Hiragana/HiraganaTheory.js
@@ -33,7 +33,7 @@ const HiraganaTheory = () => {
     
 
   return (
-    <div className="mt-2 mb-2 w-full min-h-screen flex justify-center items-center relative">
+    <div className="pt-2 pb-2 w-full min-h-screen flex justify-center items-center relative">
         <div className="flex flex-col w-24  h-7 fixed top-2 left-40">
             <button className="bg-transparent mb-4 hover:bg-gray-600 text-green-500 font-bold text-xs py-1 px-3 rounded-full border-2 border-gray-500 transition transform hover:translate-x-1" onClick={()=> dispatch(setTypeLoad(Hiragana.TYPE_LOAD_ALL))}>{"All"}</button>
             <button className="bg-transparent mb-4 hover:bg-gray-600 text-green-500 font-bold py-1 text-xs px-3 rounded-full border-2 border-gray-500 transition transform hover:translate-x-1" onClick={()=> dispatch(setTypeLoad(Hiragana.TYPE_LOAD_COMBINE))}>{"Combine word"}</button>
