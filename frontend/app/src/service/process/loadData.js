a2f56cd7806135ec26642d50d44a1a78eefe1005
Đồng Quân <quan12xz@gmail.com>
2024-01-15 21:51:50 +0700

    update

--- frontend/app/src/service/process/loadData.js
+++ frontend/app/src/service/process/loadData.js
@@ -25,4 +25,25 @@ const loadAlphabetData = async (type) => {
     return data
 }
 
+const loadQuestionData = async(type, filterType) => {
+    var strDir
+    if (type === LOAD_HIRAGANA) {
+        strDir = HIRAGANA_DATA_DIR
+    } else if (type === LOAD_KATAKANA) {
+        strDir = KATAKANA_DATA_DIR
+    } else if (type === LOAD_214KANJI_STROKE) {
+        strDir = KANJISTROKE_DATA_DIR
+    } else {
+        throw new Error("Type of data not valid")
+    }
+
+    const response = await fetch(strDir)
+    if (!response.ok) {
+        throw new Error("Fetch data fail");
+    }
+    const data = await response.json()
+
+    
+}
+
 export {loadAlphabetData, LOAD_HIRAGANA, LOAD_KATAKANA, LOAD_214KANJI_STROKE}
