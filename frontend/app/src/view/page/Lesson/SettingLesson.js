5c01241ae5e8a93f0b8bce375f84e85eaec4850a
Đồng Quân <quan12xz@gmail.com>
2024-01-26 21:36:34 +0700

    display lesson data

--- frontend/app/src/view/page/Lesson/SettingLesson.js
+++ frontend/app/src/view/page/Lesson/SettingLesson.js
@@ -19,6 +19,7 @@ const SettingLesson = () => {
             dispatch(setIsLoading(false))
         })()
     }, [])
+    console.log(lessonData);
   return (
     <div>
         <div className="w-full bg-white rounded-md shadow-md p-6">
@@ -27,53 +28,43 @@ const SettingLesson = () => {
             <div>
                 <h2 className="text-lg font-semibold mb-2">Từ vựng:</h2>
                 <ul>
-                <li className="flex items-start mb-2">
-                    <div className="mr-2">
-                    <span className="text-gray-700 japanese_word">こんにちは</span>
-                    <span className="text-sm text-gray-500">（こんにちは）</span>
-                    </div>
-                    <div>
-                    <span className="text-gray-500">Xin chào</span>
-                    <p className="text-sm text-gray-500 mt-1">Ví dụ: こんにちは、元気ですか？（Xin chào, bạn khỏe không?)</p>
-                    <p className="text-sm text-gray-500 mt-1">Ví dụ: こんにちは、元気ですか？（Xin chào, bạn khỏe không?)</p>
-                    </div>
-                </li>
-                <li className="flex items-start mb-2">
-                    <div className="mr-2">
-                    <span className="text-gray-700 kanji">ありがとう</span>
-                    <span className="text-sm text-gray-500">（ありがとう）</span>
-                    </div>
-                    <div>
-                    <span className="text-gray-500">Cảm ơn</span>
-                    <p className="text-sm text-gray-500 mt-1">Ví dụ: ありがとう、お世話になりました。（Cảm ơn, đã giúp đỡ tôi.)</p>
-                    </div>
-                </li>
+                {
+                    lessonData.vocabulary && lessonData.vocabulary.map && lessonData.vocabulary.map((word, key) => {
+                        return <li className="flex items-start mb-2" key={key}>
+                            <div className="mr-2">
+                            <span className="text-gray-700 japanese_word">{word.TypeWord === 0 ? word.WordHiragana : (word.TypeWord === 1 ? word.WordKatakana : word.WordKanji)}</span>
+                            <span className="text-sm text-gray-500">{word.TypeWord === 3 && word.WordHiragana}</span>
+                            </div>
+                            <div>
+                            <span className="text-gray-500">{word.Vietnamese}</span>
+                            {/* <p className="text-sm text-gray-500 mt-1">Ví dụ: こんにちは、元気ですか？（Xin chào, bạn khỏe không?)</p>
+                            <p className="text-sm text-gray-500 mt-1">Ví dụ: こんにちは、元気ですか？（Xin chào, bạn khỏe không?)</p> */}
+                            </div>
+                        </li>
+                    })
+                }
                 </ul>
             </div>
 
             <div className="mt-4">
                 <h2 className="text-lg font-semibold mb-2">Cấu trúc ngữ pháp:</h2>
                 <ul>
-                <li className="flex items-start mb-2">
-                    <div className="mr-2">
-                    <span className="text-gray-700 kanji">～てください</span>
-                    <span className="text-sm text-gray-500">（～てください）</span>
-                    </div>
-                    <div>
-                    <span className="text-gray-500">Làm ơn</span>
-                    <p className="text-sm text-gray-500 mt-1">Ví dụ: コーヒーを飲んでください。（Làm ơn uống cà phê.)</p>
-                    </div>
-                </li>
-                <li className="flex items-start mb-2">
-                    <div className="mr-2">
-                    <span className="text-gray-700 kanji">～ませんか</span>
-                    <span className="text-sm text-gray-500">（～ませんか）</span>
-                    </div>
-                    <div>
-                    <span className="text-gray-500">Bạn có muốn... không?</span>
-                    <p className="text-sm text-gray-500 mt-1">Ví dụ: 映画を見ませんか？（Bạn có muốn xem phim không?)</p>
-                    </div>
-                </li>
+                    {
+                        lessonData.grammar && lessonData.grammar.map && lessonData.grammar.map((grammar, key) => {
+                            return (
+                                <li className="flex items-start mb-2">
+                                    <div className="mr-2">
+                                    <span className="text-gray-700 kanji">{grammar.SentenceStructure}</span>
+                                    <span className="text-sm text-gray-500">（）</span>
+                                    </div>
+                                    <div>
+                                    <span className="text-gray-500">{grammar.Meaning}</span>
+                                    <p className="text-sm text-gray-500 mt-1">Ví dụ: {grammar.ExampleSentence} - {grammar.GrammarExampleVietnamese}</p>
+                                    </div>
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
             </div>
             </div>
